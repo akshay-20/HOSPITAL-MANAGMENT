5fb64db9ea820d3db108de738af34e77cb7d109a
akshay-20 <22bcs020@iiitdmj.ac.in>
2025-03-27 15:08:45 +0530

    Resolved merge conflicts

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 # HOSPITAL-MANAGMENT
+=======
+<<<<<<< HEAD
+# HOSPITAL-MANAGMENT
+=======
+# Hospital Mamagement System
+>>>>>>> 22560f2 (Reinitialized repository and added files)
---
+++ admin-panel.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<?php 
+include('newfunc.php');
+$con=mysqli_connect("localhost","root","","myhmsdb");
+$username = $_SESSION['username'];
+$email = $_SESSION['email'];
+$fname = $_SESSION['fname'];
+$gender = $_SESSION['gender'];
+$lname = $_SESSION['lname'];
+$contact = $_SESSION['contact'];
+
+if(isset($_POST['app-submit']))
+{
+  $username = $_SESSION['username'];
+  $email = $_SESSION['email'];
+  $fname = $_SESSION['fname'];
+  $lname = $_SESSION['lname'];
+  $gender = $_SESSION['gender'];
+  $contact = $_SESSION['contact'];
+  $doctor=$_POST['doctor'];
+  $email=$_SESSION['email'];
+  # $fees=$_POST['fees'];
+  $docFees=$_POST['docFees'];
+
+  $appdate=$_POST['appdate'];
+  $apptime=$_POST['apptime'];
+  
+
+  $query=mysqli_query($con,"insert into appointmenttb(fname,lname,gender,email,contact,doctor,docFees,appdate,apptime,userStatus,doctorStatus) values('$fname','$lname','$gender','$email','$contact','$doctor','$docFees','$appdate','$apptime','1','1')");
+  if($query)
+  {
+    echo "<script>alert('Your appointment successfully booked');</script>";
+  }
+}
+
+if(isset($_GET['cancel']))
+  {
+    $query=mysqli_query($con,"update appointmenttb set userStatus='0' where ID = '".$_GET['ID']."'");
+    if($query)
+    {
+      echo "<script>alert('Your appointment successfully cancelled');</script>";
+    }
+  }
+
+// if(isset($_POST['userAction'])){
+//   $userStatus = 0;
+//   $doctorStatus = 1;
+//   $doctor=$_POST['doctor'];
+//   $appdate=$_POST['appdate'];
+//   $apptime=$_POST['apptime'];
+//   if (($userStatus == 0) & ($doctorStatus == 1))
+//     $currentStatus = "Cancelled by you";
+//   $query="update appointmenttb set userStatus='$userStatus',doctorStatus='$doctorStatus',currentStatus='$currentStatus' where doctor='$doctor' and appdate='$appdate' and apptime='$apptime';";
+//   $result=mysqli_query($con,$query);
+//   if($result)
+//     echo("<script>alert('Appointment cancelled!');
+//     </script>");
+//     //window.location.href = 'admin-panel.php';
+// }
+
+?>
+<html lang="en">
+  <head>
+
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    
+        <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+
+    
+  
+    
+    
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+  <a class="navbar-brand" href="#"><i class="fa fa-user-plus" aria-hidden="true"></i> Global Hospital </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <style >
+    .bg-primary {
+    background: -webkit-linear-gradient(left, #3931af, #00c6ff);
+}
+.list-group-item.active {
+    z-index: 2;
+    color: #fff;
+    background-color: #342ac1;
+    border-color: #007bff;
+}
+.text-primary {
+    color: #342ac1!important;
+}
+
+.btn-primary{
+  background-color: #3c50c1;
+  border-color: #3c50c1;
+}
+  </style>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     <ul class="navbar-nav mr-auto">
+       <li class="nav-item">
+        <a class="nav-link" href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </head>
+  <style type="text/css">
+    button:hover{cursor:pointer;}
+    #inputbtn:hover{cursor:pointer;}
+  </style>
+  <body style="padding-top:50px;">
+   <div class="container-fluid" style="margin-top:50px;">
+    <h3 style = "margin-left: 40%;  padding-bottom: 20px; font-family: 'IBM Plex Sans', sans-serif;"> Welcome &nbsp<?php echo $username ?> </h3>
+    <div class="row">
+  <div class="col-md-4" style="max-width:25%; margin-top: 3%">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active" id="list-dash-list" data-toggle="list" href="#list-dash" role="tab" aria-controls="home">Dashboard</a>
+      <a class="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Book Appointment</a>
+      <a class="list-group-item list-group-item-action" href="#app-hist" id="list-pat-list" role="tab" data-toggle="list" aria-controls="home">Appointment History</a>
+      
+    </div><br>
+  </div>
+  <div class="col-md-8" style="margin-top: 3%;">
+    <div class="tab-content" id="nav-tabContent" style="width: 950px;">
+
+
+      <div class="tab-pane fade  show active" id="list-dash" role="tabpanel" aria-labelledby="list-dash-list">
+        <div class="container-fluid container-fullw bg-white" >
+              <div class="row">
+               <div class="col-sm-4" style="left: 10%">
+                  <div class="panel panel-white no-radius text-center">
+                    <div class="panel-body">
+                      <span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-terminal fa-stack-1x fa-inverse"></i> </span>
+                      <h4 class="StepTitle" style="margin-top: 5%;"> Book My Appointment</h4>
+                      <script>
+                        function clickDiv(id) {
+                          document.querySelector(id).click();
+                        }
+                      </script>                      
+                      <p class="links cl-effect-1">
+                        <a href="#list-home" onclick="clickDiv('#list-home-list')">
+                          Book Appointment
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-sm-4" style="left: 15%">
+                  <div class="panel panel-white no-radius text-center">
+                    <div class="panel-body" >
+                      <span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-paperclip fa-stack-1x fa-inverse"></i> </span>
+                      <h4 class="StepTitle" style="margin-top: 5%;">My Appointments</h2>
+                    
+                      <p class="cl-effect-1">
+                        <a href="#app-hist" onclick="clickDiv('#list-pat-list')">
+                          View Appointment History
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+      <div class="tab-pane fade" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-body">
+              <center><h4>Create an appointment</h4></center><br>
+              <form class="form-group" method="post" action="admin-panel.php">
+                <div class="row">
+                  <!--<div class="col-md-4"><label>First Name:</label></div>
+                  <div class="col-md-8"><input type="text" class="form-control" name="fname"></div><br><br>
+                  <div class="col-md-4"><label>Last Name:</label></div>
+                  <div class="col-md-8"><input type="text" class="form-control"  name="lname"></div><br><br>
+                  <div class="col-md-4"><label>Email id:</label></div>
+                  <div class="col-md-8"><input type="text"  class="form-control" name="email"></div><br><br>
+                  <div class="col-md-4"><label>Contact Number:</label></div>
+                  <div class="col-md-8"><input type="text" class="form-control"  name="contact"></div><br><br> -->
+                  <div class="col-md-4"><label for="doctor">Doctors:</label></div>
+                  <div class="col-md-8">
+                   <select name="doctor" class="form-control" id="doctor" required="required">
+                      <option value="" disabled selected>Select Doctor</option>
+                      <?php display_docs();?>
+                    </select>
+                    <script>
+              document.getElementById('doctor').onchange = function updateFees(e) {
+                document.getElementById('docFees').value = document.querySelector(`[value=${this.value}]`).getAttribute('data-value');
+              };
+            </script>
+                  </div><br><br>
+
+                  
+                  <div class="col-md-4"><label for="consultancyfees">
+                                Consultancy Fees
+                              </label></div>
+                              <div class="col-md-8">
+                              <!-- <div id="docFees">Select a doctor</div> -->
+                              <input class="form-control" type="text" name="docFees" id="docFees" readonly="readonly"/>
+                  </div><br><br>
+
+                  <div class="col-md-4"><label>Date</label></div>
+                  <div class="col-md-8"><input type="date" class="form-control datepicker" name="appdate"></div><br><br>
+
+                  <div class="col-md-4"><label>Time</label></div>
+                  <div class="col-md-8">
+                    <input type="time" class="form-control" name="apptime">
+                  </div><br><br>
+
+                  <div class="col-md-4">
+                    <input type="submit" name="app-submit" value="Create new entry" class="btn btn-primary" id="inputbtn">
+                  </div>
+                  <div class="col-md-8"></div>                  
+                </div>
+              </form>
+            </div>
+          </div>
+        </div><br>
+      </div>
+      
+<div class="tab-pane fade" id="app-hist" role="tabpanel" aria-labelledby="list-pat-list">
+        
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    
+                    <th scope="col">Doctor Name</th>
+                    <th scope="col">Consultancy Fees</th>
+                    <th scope="col">Appointment Date</th>
+                    <th scope="col">Appointment Time</th>
+                    <th scope="col">Current Status</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php 
+
+                    $con=mysqli_connect("localhost","root","","myhmsdb");
+                    global $con;
+
+                    $query = "select ID,doctor,docFees,appdate,apptime,userStatus,doctorStatus from appointmenttb where fname ='$fname' and lname='$lname';";
+                    $result = mysqli_query($con,$query);
+                    while ($row = mysqli_fetch_array($result)){
+              
+                      #$fname = $row['fname'];
+                      #$lname = $row['lname'];
+                      #$email = $row['email'];
+                      #$contact = $row['contact'];
+                  ?>
+                      <tr>
+                        <td><?php echo $row['doctor'];?></td>
+                        <td><?php echo $row['docFees'];?></td>
+                        <td><?php echo $row['appdate'];?></td>
+                        <td><?php echo $row['apptime'];?></td>
+                        
+                          <td>
+                    <?php if(($row['userStatus']==1) && ($row['doctorStatus']==1))  
+                    {
+                      echo "Active";
+                    }
+                    if(($row['userStatus']==0) && ($row['doctorStatus']==1))  
+                    {
+                      echo "Cancelled by You";
+                    }
+
+                    if(($row['userStatus']==1) && ($row['doctorStatus']==0))  
+                    {
+                      echo "Cancelled by Doctor";
+                    }
+                        ?></td>
+
+                        <td>
+                        <?php if(($row['userStatus']==1) && ($row['doctorStatus']==1))  
+                        { ?>
+
+													
+	                        <a href="admin-panel.php?ID=<?php echo $row['ID']?>&cancel=update" 
+                              onClick="return confirm('Are you sure you want to cancel this appointment ?')"
+                              title="Cancel Appointment" tooltip-placement="top" tooltip="Remove"><button class="btn btn-danger">Cancel</button></a>
+	                        <?php } else {
+
+                                echo "Cancelled";
+                                } ?>
+                        
+                        </td>
+                      </tr>
+                    <?php } ?>
+                </tbody>
+              </table>
+        <br>
+      </div>
+
+
+
+
+      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
+      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+        <form class="form-group" method="post" action="func.php">
+          <label>Doctors name: </label>
+          <input type="text" name="name" placeholder="Enter doctors name" class="form-control">
+          <br>
+          <input type="submit" name="doc_sub" value="Add Doctor" class="btn btn-primary">
+        </form>
+      </div>
+       <div class="tab-pane fade" id="list-attend" role="tabpanel" aria-labelledby="list-attend-list">...</div>
+    </div>
+  </div>
+</div>
+   </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.1/sweetalert2.all.min.js">
+   </script>
+
+
+
+  </body>
+</html>
